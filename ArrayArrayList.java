6d5ef9fb1e4b77e80af5d0878daf6d46a2fe6935
Hatim-Dahi <hatimtaherdahi53@gmail.com>
2024-05-12 14:10:31 +0530

    DynamicMemoryAllocation

--- ArrayArrayList.java
+++ ArrayArrayList.java
@@ -38,9 +38,26 @@ public class ArrayArrayList{
             System.out.println(Arrays.toString(arr[i]));
         }
     }
+    public static void dynamicArray(){
+        //arraylist is class defined in java.util package
+        ArrayList<Integer> list= new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<4;i++){
+            list.add(sc.nextInt());
+        }
+        System.out.println(list);
+        //to get first element of the list
+        System.out.println(list.get(0));
+        System.out.println(list.contains(2));
+        list.set(0, 66);
+        list.remove(1);
+        list.clear();
+        System.out.println(list.size()); 
+    }
     public static void main(String[] args) {
     //    integerArray();
     //    stringArray();
-    twoDimensionalArray();
+    // twoDimensionalArray();
+    dynamicArray();
     }
 }
