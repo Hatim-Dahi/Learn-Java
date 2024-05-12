f8ce34046fbaa92e5af3404838b4fcd4d640b90f
Hatim-Dahi <hatimtaherdahi53@gmail.com>
2024-05-12 13:42:02 +0530

    Twodimentional Array

--- ArrayArrayList.java
+++ ArrayArrayList.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class ArrayArrayList{
-    public static void IntegerArray(){
+    public static void integerArray(){
          //syntax to create an array 
        int[] arr= new int[10];
        for(int i=0;i<10;i++){
@@ -14,7 +14,7 @@ public class ArrayArrayList{
        //another simple way to print
        System.out.println(Arrays.toString(arr));
     }
-    public static void StringArray(){
+    public static void stringArray(){
         String[] str= new String[5];
         Scanner sc= new Scanner(System.in);
          for(int i=0;i<5;i++){
@@ -25,8 +25,22 @@ public class ArrayArrayList{
        str[2]= "hatim";
        System.out.println(Arrays.toString(str));
     }
+    
+    public static void twoDimensionalArray(){
+        int[][] arr= new int[3][3];
+        Scanner sc= new Scanner(System.in);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                arr[i][j]= sc.nextInt();
+            }
+        }
+        for(int i=0;i<3;i++){
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
     public static void main(String[] args) {
-       IntegerArray();
-       StringArray();
+    //    integerArray();
+    //    stringArray();
+    twoDimensionalArray();
     }
 }
